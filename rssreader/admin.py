945096d1ea42f116d29748ec3b0cb8fcfc2676a0
CKiilu <cndeti@gmail.com>
2016-03-27 23:07:01 +0300

    Parsed rss file

--- rssreader/admin.py
+++ rssreader/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import SourceUrl
+
 # Register your models here.
+admin.site.register(SourceUrl)
--- rssreader/views.py
+++ rssreader/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from .models import SourceUrl
+
+from django.http import HttpResponse
+
+import feedparser
+
 # Create your views here.
+def index(request):
+	feeds = SourceUrl.objects.all()
+	for objects in feeds:
+		objects.url = feedparser.parse(objects.url + ".rss")
+	context = {
+		"feeds": feeds,
+
+	}
+	return render(request, "index.html", context)
