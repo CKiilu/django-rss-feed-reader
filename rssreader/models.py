41e23dac87342fac5696a34f1a19c38317e79951
CKiilu <cndeti@gmail.com>
2016-03-28 00:41:08 +0300

    Make fields unique

--- rssreader/models.py
+++ rssreader/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 # Create your models here.
 class SourceUrl(models.Model):
-	name = models.CharField(max_length = 100)
-	url = models.URLField(max_length = 200)
+	name = models.CharField(max_length = 100, unique = True)
+	url = models.URLField(max_length = 200, unique = True)
 	last_visited = models.DateTimeField(auto_now = True)
 
 	def __unicode__(self):
