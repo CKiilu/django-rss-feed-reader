520b307f1ca2c1b58d927281421c1b74c3b6f027
CKiilu <cndeti@gmail.com>
2016-04-01 14:40:15 +0300

    Used beautifulsoup to prettify rss text

--- rssreader/views.py
+++ rssreader/views.py
@@ -7,6 +7,9 @@ from django.http import HttpResponse
 
 import feedparser
 
+from bs4 import BeautifulSoup
+
+
 # Create your views here.
 def index(request):
 	form = FeedForm(request.POST or None)
@@ -16,19 +19,28 @@ def index(request):
 		print (form.cleaned_data.get("name"))
 		instance.save()
 	feeds = SourceUrl.objects.all()
+   
 	for objects in feeds:
 		objects.url = feedparser.parse(objects.url)
 		objects.url = objects.url.entries[0].summary_detail.value
+		soup = BeautifulSoup(objects.url, 'html.parser')
+		objects.url = soup.prettify()
+		
 	context = {
 		"feeds": feeds,
 		"form": form,
 	}
 	return render(request, "index.html", context)
+
 def details(request, id = None):
 	feeds = get_object_or_404(SourceUrl, id = id)
 	
 	feeds.url = feedparser.parse(feeds.url)
 	feeds.url = feeds.url.entries
+
+	for objects in feeds.url:
+		soup = BeautifulSoup(objects.summary, 'html.parser')
+		objects.summary = soup.prettify()
 
 	context = {
 		"feeds": feeds,
